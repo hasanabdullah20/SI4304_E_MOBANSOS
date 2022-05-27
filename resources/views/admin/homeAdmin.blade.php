dcfd9f0224d111f8936c3b87ef878e8697ad6183
ntaulya <netaaulya@gmail.com>
2022-05-27 23:54:39 +0700

    ubah tampilan admin

--- resources/views/admin/homeAdmin.blade.php
+++ resources/views/admin/homeAdmin.blade.php
@@ -1,11 +1,18 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="p-5" id="card">
-    <p class="text-white fw-bold" style="font-size: 35px">Penambahan Akun RT</p>
-    <p class="text-white" style="font-size: 18px;">Klik disini untuk menambahkan RT Baru</p>
-    <div class="pt-5">
-        <a href="/tambhRT" class="btn btn-info d-block text-white">Tambah RT</a>
+<div class="container">
+    <div class="row">
+      <div class="col-sm-6">
+        <h3>Penambahan Akun RT</h3>
+        <p class="text" style="font-size: 18px;">Klik disini untuk menambahkan RT baru</p>
+        <div class="pt-3">
+            <a href="/requestWarga" class="btn btn-info d-block text-white">Tambah RT</a>
+        </div>
+      </div>
+      <div class="col-sm-6">
+        <img src="images/mobansos-home.png" style="margin-left: 70px">
+      </div>
     </div>
 </div>
 @endsection
